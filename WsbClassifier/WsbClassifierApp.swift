180c4f9e19770117a010023e773edc42d950a741
ifrankim <fkgaieski@gmail.com>
2023-11-23 10:28:20 +0900

    Initial Commit

---
+++ WsbClassifier/WsbClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  WsbClassifierApp.swift
+//  WsbClassifier
+//
+//  Created by Francisco Gaieski on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WsbClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
